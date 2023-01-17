ca68cb7f87885367d4ac64f3cbb00d53995a3511
Pixel48 <piksel48@gmail.com>
2023-01-17 10:22:01 +0100

    Update Signer classes

--- src/components/Signer.jsx
+++ src/components/Signer.jsx
@@ -1,18 +1,32 @@
 import firebase from "firebase/compat/app"; // firebase SDK
 import { auth } from "../firebase";
 
+const signerButtonClasses = "btn btn-primary btn-lg btn-block m-2 p-20";
+
 const SignIn = () => {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   };
 
-  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+  return (
+    <button
+      className={signerButtonClasses + " green"}
+      onClick={signInWithGoogle}>
+      Sign in with Google
+    </button>
+  );
 };
 
 const SignOut = () => {
   return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+    auth.currentUser && (
+      <button
+        className={signerButtonClasses + " red"}
+        onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
   );
 };
 
